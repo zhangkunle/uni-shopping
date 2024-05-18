21b061cfa323c2e74772abd297a76b3b15637993
zhangkl <3313651836@qq.com>
2024-05-18 11:17:00 +0800

    finish goodslist

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,71 @@
+<template>
+  <view class="goods_item" @click="getGoodDetails(goods.goods_id)">
+    <view class="goods-item-left">
+      <image :src="goods.goods_small_logo || defaultImg" class="goods_img"></image>
+    </view>
+    <view class="goods_content">
+      <view class="goods_title">
+        {{goods.goods_name}}
+      </view>
+      <view class="goods_price">
+        ￥{{Number(goods.goods_price).toFixed(2)}}
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    props: {
+      goods: {
+        type: Object,
+        default: {}
+      },
+    },
+    data() {
+      return {
+        defaultImg: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      }
+    },
+    methods:{
+      getGoodDetails(id){
+        uni.navigateTo({
+          url:'/subpkg/goods_details/goods_details?goods_id=' + id
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .goods_item {
+    display: flex;
+    width: 100%;
+    margin-top: 20rpx;
+
+    .goods-item-left {
+      margin-right: 10rpx;
+
+      .goods_img {
+        width: 200rpx;
+        height: 200rpx;
+        display: block;
+      }
+    }
+
+    .goods_content {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goods_title {
+        font-size: 30rpx;
+      }
+
+      .goods_price {
+        font-size: 32rpx;
+        color: red;
+      }
+    }
+  }
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,22 +1,62 @@
 <template>
-  <view>
-    哈哈哈哈
+  <view class="goods_list_box">
+    <block v-for="(item,i) in goodsList" :key="i">
+      <my-goods :goods="item"></my-goods>
+    </block>
   </view>
 </template>
 
 <script>
+  import {
+    $http
+  } from '@escook/request-miniprogram'
+  uni.$http = $http
+  $http.baseUrl = 'https://api-hmugo-web.itheima.net'
+  import showMsg from '../../contant/showMsg.js'
   export default {
+    onLoad(options) {
+      this.requestQuery.query = options.query || ''
+      this.requestQuery.cid = options.cid || ''
+      this.getGoodsList()
+    },
     data() {
       return {
-        
+        requestQuery: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        goodsList: [],
+        total: 0,
+        isLoading: false
       }
     },
     methods: {
-      
+      async getGoodsList(cb) {
+        this.isLoading = true
+        const res = await $http.get('/api/public/v1/goods/search', this.requestQuery)
+        console.log(res)
+        this.isLoading = false
+        cb && cb()
+        if (res.data.meta.status === 200) {
+          this.goodsList = [...this.goodsList, ...res.data.message.goods]
+          this.total = res.data.message.total
+        }
+      }
+    },
+    onReachBottom() {
+      if (this.requestQuery.pagenum * this.requestQuery.pagesize > this.total) showMsg('已加载完全部数据！')
+      if (this.isLoading) return
+      this.requestQuery.pagenum += 1
+      this.getGoodsList()
+    },
+    onPullDownRefresh() {
+      this.requestQuery.pagenum = 1
+      this.total = 0
+      this.isloading = false
+      this.goodsList = []
+      this.getGoodsList(()=>uni.stopPullDownRefresh())
     }
   }
 </script>
-
-<style>
-
-</style>
